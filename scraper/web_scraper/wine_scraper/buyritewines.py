9e8a372179ddc7269b9fc3323f5041a4ad5d9e78
vincentzhchen <vincent.zh.chen@gmail.com>
2020-04-21 23:58:00 -0400

    clean up

--- scraper/web_scraper/wine_scraper/buyritewines.py
+++ scraper/web_scraper/wine_scraper/buyritewines.py
@@ -9,46 +9,45 @@ import pandas as pd
 # PROJECT LIB
 from scraper.settings import settings
 
-HTML = "https://www.buyritewines.com/wines/"
-HTML_FORMAT = HTML + "?page={}&l={}"  # need a page number and result size
+HTML = "https://www.buyritewines.com/"
+HTML_FORMAT = HTML + "wines/?page={}&l={}"  # need a page number and result size
+
+RESULTS_PER_PAGE = 100  # easier if constant
 
 
-def query_one_page(page_num, results_per_page):
-    print("QUERYING page {} with {} results per page".format(
-        page_num, results_per_page))
-    query = pd.read_html(HTML_FORMAT.format(page_num, results_per_page))
-    df = pd.concat(query)
+def query_one_page(page_num):
+    print("QUERYING page {}".format(page_num))
+    query = HTML_FORMAT.format(page_num, RESULTS_PER_PAGE)
+    result = pd.read_html(query)
+    df = pd.concat(result)
     if 4 not in df:
         print("NO DATA found, returning empty df.")
         return pd.DataFrame()
 
     df = df.loc[df[4].notnull(), [4]]  # data is in every few rows
     df.columns = ["RAW_DATA_STR"]
-    df["PAGE_NUM"] = page_num
+
+    df["SRC"] = HTML
+    df["QUERY"] = query
+    df["AS_OF_DATE"] = pd.to_datetime("today")
 
     df = get_metadata_from_query_result(df)
-    df = df[df["NAME"].notnull()].reset_index(drop=True)
 
-    df = df[["NAME", "PRICE", "BASE_PRICE", "IS_ON_SALE"]]
+    df = df[["NAME", "PRICE", "BASE_PRICE", "IS_ON_SALE", "SRC", "QUERY",
+             "AS_OF_DATE"]]
     return df
 
 
-def query_all_pages(results_per_page, max_page_num=None):
+def query_all_pages():
     all_dfs = []
-    if max_page_num is not None:
-        for page_num in range(1, max_page_num + 1):
-            df = query_one_page(page_num, results_per_page)
+    page_num = 1
+    while True:
+        df = query_one_page(page_num)
+        if not df.empty:
             all_dfs.append(df)
-
-    else:
-        page_num = 1
-        while True:
-            df = query_one_page(page_num, results_per_page)
-            if not df.empty:
-                all_dfs.append(df)
-                page_num += 1
-            else:
-                break
+            page_num += 1
+        else:
+            break
 
     return pd.concat(all_dfs, ignore_index=True)
 
@@ -57,24 +56,31 @@ def get_metadata_from_query_result(df):
     df["NAME"] = df["RAW_DATA_STR"].str.findall("content_name.*?,").apply(
         lambda x: x[0].split(":")[1][:-1].strip() if x else None)
 
-    df["PRICE"] = df["RAW_DATA_STR"].str.findall("item_price.*?\}").apply(
-        lambda x: x[0].split(":")[1][:-2].replace(",", "") if x else None)
-    df["PRICE"] = df["PRICE"].astype(float)
+    df = df[df["NAME"].notnull()].copy()
 
-    df["BASE_PRICE"] = df["RAW_DATA_STR"].str.findall("\(Reg.*?\)").apply(
-        lambda x: x[0].split("$")[1][:-1].replace(",", "") if x else None)
-    df["BASE_PRICE"] = df["BASE_PRICE"].astype(float)
+    # this should return a list of one price [reg. price] if no sale and
+    # two prices [sale price, reg. price] if sale
+    regex = "\$\d{1,3}(?:[,]\d{3})*(?:[.]\d{2})"
+    """
+            |--|------|------------|-----------|
+            |$ | 1-3  | 3 digits   | 2 digits  |
+            |  |digits| repeated   |  (cents)  |
+            |  |      |            |           |
+    """
+    df["PRICES"] = df["RAW_DATA_STR"].str.findall(regex)
+    df["PRICE"] = df["PRICES"].apply(lambda x: x[0][1:].replace(",", ""))
+    df["BASE_PRICE"] = df["PRICES"].apply(
+        # if one price, then take the single listed price
+        lambda x: x[1][1:].replace(",", "") if (
+            x[1:2]) else x[0][1:].replace(",", ""))
 
-    df["BASE_PRICE"] = df["PRICE"].where(df["BASE_PRICE"].isnull(),
-                                         df["BASE_PRICE"])
-
-    df["IS_ON_SALE"] = df["BASE_PRICE"] != df["PRICE"]
+    df["IS_ON_SALE"] = df["BASE_PRICE"] > df["PRICE"]
 
     return df
 
 
 if __name__ == "__main__":
-    df = query_all_pages(100)
+    df = query_all_pages()
     now = pd.to_datetime("today").strftime("%Y%m%d_%H%M%S%f")
     out_file = "buyritewines_catalog_{}.csv".format(now)
-    df.to_csv(os.path.join(settings.DATA_DIRECTORY, out_file))
+    df.to_csv(os.path.join(settings.DATA_DIRECTORY, out_file), index=False)
