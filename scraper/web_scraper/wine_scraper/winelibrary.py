5577f3f70abf81b59c235ffe07c955969c94ef01
vincentzhchen <vincent.zh.chen@gmail.com>
2020-04-21 22:35:38 -0400

    clean up, added more metadata

--- scraper/web_scraper/wine_scraper/winelibrary.py
+++ scraper/web_scraper/wine_scraper/winelibrary.py
@@ -11,21 +11,29 @@ import requests
 # PROJECT LIB
 from scraper.settings import settings
 
-HTML = "https://winelibrary.com/search"
-HTML_FORMAT = HTML + "?page={}"  # need a page number
+HTML = "https://winelibrary.com/"
+HTML_FORMAT = HTML + "search?page={}"  # need a page number
 
 
 def query_one_page(page_num):
     print("QUERYING page {}".format(page_num))
-    response = requests.get(HTML_FORMAT.format(page_num))
+    query = HTML_FORMAT.format(page_num)
+    response = requests.get(query)
     html_str = response.text
     xml_raw_product_data = html_str.split("product_id_")[1:]
     df = pd.DataFrame(xml_raw_product_data, columns=["RAW_DATA_STR"])
+    if df.empty:
+        print("NO DATA found, returning empty df.")
+        return pd.DataFrame()
+
+    df["SRC"] = HTML
+    df["QUERY"] = query
+    df["AS_OF_DATE"] = pd.to_datetime("today")
 
     df = get_metadata_from_query_result(df)
-    df = df[df["NAME"].notnull()].reset_index(drop=True)
 
-    df = df[["NAME", "PRICE", "BASE_PRICE", "IS_ON_SALE"]]
+    df = df[["NAME", "PRICE", "BASE_PRICE", "IS_ON_SALE", "SRC", "QUERY",
+             "AS_OF_DATE"]]
     return df
 
 
@@ -70,4 +78,4 @@ if __name__ == "__main__":
     df = query_all_pages()
     now = pd.to_datetime("today").strftime("%Y%m%d_%H%M%S%f")
     out_file = "winelibrary_catalog_{}.csv".format(now)
-    df.to_csv(os.path.join(settings.DATA_DIRECTORY, out_file))
+    df.to_csv(os.path.join(settings.DATA_DIRECTORY, out_file), index=False)
