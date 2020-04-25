8d6198b00e912833bef9c5e250b3f394d3a3dced
vincentzhchen <vincent.zh.chen@gmail.com>
2020-04-25 17:18:59 -0400

    add unit testing

---
+++ scraper/tests/web_scraper/test_allendalewine.py
@@ -0,0 +1,8 @@
+"""All unit tests for allendalewine module.
+
+"""
+
+from scraper.web_scraper.wine_scraper import allendalewine
+
+def test_scrape():
+    allendalewine.scrape()
---
+++ scraper/tests/web_scraper/test_buyritewines.py
@@ -0,0 +1,8 @@
+"""All unit tests for buyritewines module.
+
+"""
+
+from scraper.web_scraper.wine_scraper import buyritewines
+
+def test_scrape():
+    buyritewines.scrape()
