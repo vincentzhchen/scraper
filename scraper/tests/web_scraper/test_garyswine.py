cbf56ac76de9ad29515761d44113eac41fbca764
vincentzhchen <vincent.zh.chen@gmail.com>
2020-04-25 20:01:41 -0400

    more unit tests

---
+++ scraper/tests/web_scraper/test_garyswine.py
@@ -0,0 +1,8 @@
+"""All unit tests for garyswine module.
+
+"""
+
+from scraper.web_scraper.wine_scraper import garyswine
+
+def test_scrape():
+    garyswine.scrape()
---
+++ scraper/tests/web_scraper/test_winelibrary.py
@@ -0,0 +1,8 @@
+"""All unit tests for winelibrary module.
+
+"""
+
+from scraper.web_scraper.wine_scraper import winelibrary
+
+def test_scrape():
+    winelibrary.scrape()
